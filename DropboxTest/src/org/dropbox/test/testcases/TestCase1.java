073b4c3e0db4127c806a39f9c84f0f2860b99f5d
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2016-04-13 18:28:15 +0300

    added ini file

---
+++ DropboxTest/src/org/dropbox/test/testcases/TestCase1.java
@@ -0,0 +1,17 @@
+package org.dropbox.test.testcases;
+
+import org.dropbox.test.basetest.BaseTestCase;
+import org.testng.annotations.Test;
+
+
+public class TestCase1 extends BaseTestCase {
+	
+	/** The ini file, with values for this testcase */
+	String iniFile = "/resources/data/testcases/TestCase1.ini";
+
+	@Test
+	public void testCase1() throws InterruptedException {
+		System.out.println("in test method");
+	}
+
+}
