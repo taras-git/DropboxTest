3379ea2a3624a57ab2dfec31e669b7dab21b25d5
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2016-04-13 17:51:02 +0300

    added new utils class

---
+++ DropboxTest/src/org/dropbox/test/utils/Utils.java
@@ -0,0 +1,64 @@
+package org.dropbox.test.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/**
+ * The Class Utils. Holds the static methods to work with Property files, and
+ * retrieves the value based on appropriate key.
+ *
+ * @author Taras Tymchyshyn
+ */
+public class Utils {
+
+	/**
+	 * Gets the absolute path of the working directory.
+	 *
+	 * @return the absolute path
+	 */
+	private static Path getAbsolutePath() {
+		return Paths.get("").toAbsolutePath();
+	}
+
+	/**
+	 * Gets the value from *.ini file, based on appropriate key.
+	 *
+	 * @param key
+	 *            the key
+	 * @param path
+	 *            the path to the ini file
+	 * @return the value
+	 */
+	public static String getIniFileValue(String key, String path) {
+		Properties prop = new Properties();
+		InputStream input = null;
+		String value = null;
+
+		try {
+
+			input = new FileInputStream(getAbsolutePath() + path);
+			// load a properties file
+			prop.load(input);
+			// get the property value
+			value = prop.getProperty(key);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// return the value in case of success
+		return value;
+	}
+}
