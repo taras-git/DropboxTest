f71e6342f9e5d3c62e2bc97c7fcd431bb682fb61
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2016-04-15 22:48:53 +0300

    new class

---
+++ DropboxTest/src/org/dropbox/test/testcases/DropboxCleanUp.java
@@ -0,0 +1,20 @@
+package org.dropbox.test.testcases;
+
+import org.dropbox.test.basetest.BaseTestCase;
+import org.dropbox.test.utils.Utils;
+import org.testng.annotations.Test;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.files.ListFolderErrorException;
+
+
+public class DropboxCleanUp extends BaseTestCase {
+
+	@Test
+	public void dropboxCleanUp() throws ListFolderErrorException, DbxException {
+		System.out.println("-->in test method: " + getClass().getName());
+				
+		Utils.getItems(true);
+		Utils.cleanUpDropbox();
+	}
+}
